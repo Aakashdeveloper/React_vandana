2a97791d0826a3480473b58505901903123e7baa
Avyaan <avi@Avyaans-MacBook-Pro.local>
2018-09-26 01:34:06 +0100

    a

---
+++ second/src/components/postitem.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class PostItem extends Component{
+
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return <div>This is detail page</div>
+    }
+}
+
+export default PostItem;
+
+
+
--- second/src/index.js
+++ second/src/index.js
@@ -1,5 +1,11 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Link} from 'react-router-dom';
+
+import Posts from './components/posts';
+import Profile from './components/profile';
+import PostItem from './components/postitem.js';
+
 
 class App extends Component{
     render(){
@@ -15,5 +21,43 @@ class App extends Component{
     }
 }
 
-ReactDOM.render(<App/>,document.getElementById('root'))
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+            <header>
+            <nav class="navbar navbar-inverse">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span> 
+                    </button>
+                    <a class="navbar-brand" >WebSiteName</a>
+                    </div>
+                    <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav">
+                        <li><Link to="/">Home</Link></li>
+                        <li> <Link to="/posts">Posts</Link></li> 
+                        <li><Link to="/profile">Profile</Link></li> 
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                        
+                    </ul>
+                    </div>
+                </div>
+                </nav>
+                
+               
+                
+            </header>
+            <Route exact path="/" component={App}></Route>
+            <Route exact path="/posts" component={Posts}></Route>
+            <Route exact path="/profile" component={Profile}></Route>
+            <Route exact path="/posts/1" component={PostItem}></Route>
+        </div>
+    </BrowserRouter>
+    ,document.getElementById('root'))
 
