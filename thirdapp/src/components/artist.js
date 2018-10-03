4e98d37f776ce1018b855e2846d96a493671f8ef
Avyaan <avi@Avyaans-MacBook-Pro.local>
2018-10-03 05:48:19 +0530

    a

---
+++ thirdapp/src/components/artist.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import Header from './header'
+
+const REQ_URL = `http://localhost:6700/artists`
+
+class Artists extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            artist:''
+        }
+    }
+
+    componentDidMount(){
+        // console.log(this.props)
+        fetch(`${REQ_URL}/${this.props.match.params.artistid}`,{
+            method:'GET'
+        })
+        .then(response => response.json())
+        .then(json => {
+            this.setState({
+                artist:json
+            })
+        })
+
+    }
+    render(){
+        return(
+            <div>
+                <Header></Header>
+                <div className="artist_bio">
+                    <div className="artist_image">
+                       <span style={{background:`url('/images/covers/${this.state.artist.cover}.jpg') no-repeat`}}>
+                       </span>
+                    </div>
+                    <div className="bio">
+                        <h3>{this.state.artist.name}</h3>
+                        <div className="bio_text">
+                            {this.state.artist.bio}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Artists;
